00c3ccc81dce07bee460ab097c1b62900d388c0f
Rishabh Jain <rjain_be19@thapar.edu>
2020-07-17 00:00:06 +0530

    deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const cors = require("cors");
-
+const path = require("path");
 //Config
 const keys = require("./config/keys");
 require("./config/passportConfig");
