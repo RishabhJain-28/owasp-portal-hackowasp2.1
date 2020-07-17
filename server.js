dc1270ff5a67645f89217a017a6f139a96d0ec6e
Rishabh Jain <rjain_be19@thapar.edu>
2020-07-17 12:11:40 +0530

    deploy

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 app.use(express.static("./client/build"));
+app.use(express.static("./public"));
 
 // Importing Routes
 const team = require("./routes/team");
@@ -65,7 +66,7 @@ mongoose
 // Port Setup
 
 app.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 const port = process.env.PORT || 5000;
